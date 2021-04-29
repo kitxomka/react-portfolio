44ca7c760309e80689bcc697f25906e4a439d178
kitxomka <yanovsky.alena@gmail.com>
2021-04-29 15:53:54 +0300

    finish touches

--- src/components/projects-timeline/projects-data.jsx
+++ src/components/projects-timeline/projects-data.jsx
@@ -1,31 +1,44 @@
-import L_ReactToDoList from "../../assets/img/projects/React_ToDo_List.png";
-import L_MernTipCalc from "../../assets/img/projects/mern_tip_calc.png";
+import L_ReactToDoList from "../../assets/img/projects/react_todo.png";
+import L_ReactKetoRecipes from "../../assets/img/projects/react_keto_recipes.png";
+import L_ReactPortfolio from "../../assets/img/projects/react_portfolio.png";
 
 export const projects = [
     {
         id: '1',
-        projectName: 'React ToDo App',
-        date: "01/10/2020",
-        imgSrc: L_ReactToDoList,
-        imgAltText: "React ToDo App",
-        description: "This is a Todo App created with React and Material UI that keeps a track of your Todos",
-        link: "https://akjha96.github.io/Todo-List-React/",
-        code: "https://github.com/akjha96/Todo-List-React",
-        features: ['Keeps track of your Todos', 'Powered by React and Material UI', 'Respoisive Design'],
-        techUsed: ['Html 5', 'Css 3', 'React', 'Material UI'],
+        projectName: 'React Keto Recipes Site',
+        date: "28/04/2021",
+        imgSrc: L_ReactKetoRecipes,
+        imgAltText: "React Keto Recipes Site",
+        description: "This site is about ketogenic (keto) recipes and offers a variety of delicious recipes (In dev state)",
+        link : "http://157.245.71.103/react_keto_recipes/build/#/",
+        code: "https://github.com/kitxomka/react_keto_recipes",
+        features: ['Make and enjoy delicious food', 'Styled with  Material-UI', 'Respoisive Design'],
+        techUsed: ['React', 'Material UI'],
 
     },
     {
         id: '2',
-        projectName: 'MERN Tip Calculator',
-        date: "16/09/2020",
-        imgSrc: L_MernTipCalc,
-        imgAltText: "MERN Tip Calculator",
-        description: "This app calculates Tip for the amount entered and percentage of tip to be given. It uses MERN stack build to acomplish the same.",
-        link : 'https://github.com/akjha96/MernTipCaculator-FrontEnd',
-        code: "https://github.com/akjha96/MernTipCaculator-FrontEnd",
-        features: ['Enter amount upto your desire', 'Real-time API calls with Backend features', 'Styled with  Material-UI'],
-        techUsed: ['Node.js', 'Express', 'React', 'Material UI', 'MongoDB'],
-
+        projectName: 'React ToDo App',
+        date: "12/03/2021",
+        imgSrc: L_ReactToDoList,
+        imgAltText: "React ToDo App",
+        description: "This is a Todo App keeps a track of your Todos",
+        link: "http://157.245.71.103/react-todo/build/",
+        code: "https://github.com/kitxomka/react-todo",
+        features: ['Keeps track of your Todos', 'Powered by React and Material UI', 'Respoisive Design', 'Saving the tasks'],
+        techUsed: ['React', 'Material UI'],
     },
+    {
+        id: '3',
+        projectName: 'The Portfolio',
+        date: "20/02/2021",
+        imgSrc: L_ReactPortfolio,
+        imgAltText: "React ToDo App",
+        description: "This Portfolio App is your way to know me a litter better before meeting.",
+        link: "http://157.245.71.103/react-portfolio/build/",
+        code: "https://github.com/kitxomka/react-portfolio",
+        features: ['Telling a little bit about myself', 'My Tech Skills', 'My Projects (not all of them)', 'My contacts'],
+        techUsed: ['React', 'React Bootstrap'],
+    }
+    
 ]
