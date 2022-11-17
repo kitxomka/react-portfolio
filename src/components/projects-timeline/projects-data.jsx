dd1a18367024d0732696de1eab066264ac93ad43
kitxomka <yanovsky.alena@gmail.com>
2022-11-17 09:09:13 +0200

    some changes

--- src/components/projects-timeline/projects-data.jsx
+++ src/components/projects-timeline/projects-data.jsx
@@ -3,6 +3,9 @@ import L_ReactKetoRecipes from "../../assets/img/projects/react_keto_recipes.png
 import L_ReactPortfolio from "../../assets/img/projects/react_portfolio.png";
 import L_TypeScriptCalculator from "../../assets/img/projects/typescript-calculator.png";
 
+const DOMAIN = '188.166.71.102';
+
+
 export const projects = [
     {
         id: '1',
@@ -23,7 +26,8 @@ export const projects = [
         imgSrc: L_ReactKetoRecipes,
         imgAltText: "React Keto Recipes Site",
         description: "This site is about ketogenic (keto) recipes and offers a variety of delicious recipes (In dev state)",
-        link : "http://alonashot.top/react_keto_recipes/build/#/",
+        // link : "http://alonashot.top/react_keto_recipes/build/#/",
+        link : "http://188.166.71.102/react_keto_recipes/build/#/",
         code: "https://github.com/kitxomka/react_keto_recipes",
         features: ['Make and enjoy delicious food', 'Styled with  Material-UI', 'Respoisive Design'],
         techUsed: ['React', 'Redux', 'Redux-Toolkit' , 'Material UI'],
@@ -36,7 +40,8 @@ export const projects = [
         imgSrc: L_ReactToDoList,
         imgAltText: "React ToDo App",
         description: "This is a Todo App keeps a track of your Todos",
-        link: "http://alonashot.top/react-todo/build/",
+        // link: "http://alonashot.top/react-todo/build/",
+        link: "http://188.166.71.102/react-todo/build/",
         code: "https://github.com/kitxomka/react-todo",
         features: ['Keeps track of your Todos', 'Powered by React and Material UI', 'Respoisive Design', 'Saving the tasks to local storage'],
         techUsed: ['React', 'Material UI'],
@@ -48,7 +53,8 @@ export const projects = [
         imgSrc: L_ReactPortfolio,
         imgAltText: "React ToDo App",
         description: "This Portfolio App is your way to know about me and my skills a little bit better before meeting.",
-        link: "http://alonashot.top/react-portfolio/build/",
+        // link: "http://alonashot.top/react-portfolio/build/",
+        link: "http://188.166.71.102/react-portfolio/build/",
         code: "https://github.com/kitxomka/react-portfolio",
         features: ['Telling a little bit about myself', 'My Tech Skills', 'My Projects (not all of them)', 'My contacts'],
         techUsed: ['React', 'React Bootstrap'],
